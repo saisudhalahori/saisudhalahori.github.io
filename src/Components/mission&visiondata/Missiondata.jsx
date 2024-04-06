b9a5b153e9658a32d10a6b4e10819ebcd5415fe2
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-04-06 15:14:23 +0530

    Update Missiondata.jsx

--- src/Components/mission&visiondata/Missiondata.jsx
+++ src/Components/mission&visiondata/Missiondata.jsx
@@ -10,11 +10,11 @@ const Missiondata = () => {
 <h1 className='page_contenet_heading'>Our Mission</h1>
 
 <p className='page_contenet_para'>To facilliatate sustainable development by promoting appropriate and environment frindly technologies for livelihood management..</p>
-<img src="/public/leafimage.png" className='mission_img' alt="image" />
+<img src="/leafimage.png" className='mission_img' alt="image" />
 <h1 className='page_contenet_heading'>Our Vision</h1>
 
 <p className='page_contenet_para'>To establish ourselves as responsible prime movers for developing indigenous solutions for problems and chgallenges of our environment and economy.</p>
-<img src="/public/leafimage.png" className='mission_img' alt="image" />
+<img src="/leafimage.png" className='mission_img' alt="image" />
     </div>
 
     </div>
