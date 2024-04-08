35a8defcd42718ce70ba3aab77c7a2c3b54b968b
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-04-08 13:19:16 +0530

    Update HomeContainer.jsx

--- src/Components/homecontainer/HomeContainer.jsx
+++ src/Components/homecontainer/HomeContainer.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HomeContainer.scss'
 
 const HomeContainer = () => {
+  const [show,setShow] = useState(false)
 const handleClick = (e) => {
-    e.target.value;
+    setShow(prv=>!prv)
 }
   return (
     <div className='homeContainer'>
    <h1 className='home_heading'>Sustainable Options</h1>
-   <p className='home_page'>“Environment and Energy Management Group” is a Bhopal based organisation <br/>working for promotion of bamboo based technologies for sustainable<br/> development , clean energy technologies for renewable energy generation<br/> and conservation of environment for over 20 years.</p>
-   <button className="btnbtn" onClick={handleClick}>Read More</button>
+   <p className='home_page'>“Environment and Energy Management Group” is a Bhopal based organisation working for promotion of bamboo based technologies for sustainable development, clean energy technologies for renewable energy generation and conservation of environment for over 20 years.
+   {show && <span><br/>“Environment and Energy Management Group” is a Bhopal based organisation <br/>working for promotion of bamboo based technologies for sustainable<br/> development , clean energy technologies for renewable energy generation<br/> and conservation of environment for over 20 years.</span>}
+   
+   </p>
+   <button className="btnbtn" onClick={handleClick}>{show ? "Read Less": "Read More"}</button>
         </div>
 
   )
