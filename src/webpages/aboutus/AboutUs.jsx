a134c805a0be06b0440517d0cff807b87aafe12f
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-04-08 15:17:48 +0530

    Update AboutUs.jsx

--- src/webpages/aboutus/AboutUs.jsx
+++ src/webpages/aboutus/AboutUs.jsx
@@ -12,61 +12,212 @@ const AboutUs = () => {
     
        <WebNavbar />
       <AboutusContainer />
-       <div className='aboutus-details'>
-       
-    <h1>Who we are <br/> <img className='service_cntainer_image'  src="./leafimage.png" alt="" /></h1>
-    <p>
-     Sustainable Options is a Bhopal, Madhya Pradesh, India based organization working for promotion of clean energy
-
-technologies for renewable energy generation, conservation of environment and promotion of bamboo based-
-technologies for sustainable development for over 30 years. We provide consultancy services for carbon footprint
-
-mapping , carbon offset projects for climate change mitigation ,facilitation of carbon credits , power generation
-from clean and renewable sources of energy such as bamboo gasification & combustion, small hydro power, solar
-photo voltaic power systems, wind power , production of green hydrogen and alternative fuels for replacing fossil
-fuels.
+      <div className="about-details">
+        <h1>Who we are <br/><img className='service_cntainer_image'  src="./leafimage.png" alt="" /></h1>
+        <h3>Renewable Energy, Sustainable Development & Climate Change Mitigation</h3>
+        <p>Sustainable Options is a Bhopal based organization working for promotion of clean energy
+technologies for renewable energy generation, conservation of environment and promotion of
+bamboo based-technologies for sustainable development for over 30 years.</p>
+<h3>Mission, Vision and Core Values</h3>
+<p>Our Mission: Our raison d'être is to reimagine and restructure existing strategies and use of
+resources to support our clients with their Transition to Clean Sources of Energy , ESG, SDG & Net
+Zero Carbon goals through sustainable and profitable solutions
+Our Vision: To establish ourselves as the market leaders in Switching to Clean Fuel Regime,
+Renewable Energy Management ,Carbon Credit Facilitation and Net Zero Carbon achievement for
+our clients and to promote strategies for high-growth Circular Economy business models
+</p>
+<h3>The Team</h3>
+<p>•Rahul Saksena – Chief Executive Officer<br/>
+- Founder of EEMG & Sustainable Options<br/>
+- Empanelled with Consultancy Development Center, Govt. of India (RE Projects)<br/>
+- Consultant for RE Power Projects<br/>
+- Consultant to IIT Roorkee<br/>
+- Supply Chain Management Expert with National Bamboo Mission and M.P. State Bamboo
+Mission<br/>
+- Biomass Supply Chain Management Expert with the World Resource Institute, New Delhi<br/>
+- Empanelled Consultant with M.P. Urja Vikas Nigam for Climate Change Mitigation & RE
+Projects<br/>
+- Consultant for Carbon Footprint Assessment & Reduction Strategies<br/>
+• Dev Raghav – Managing Director<br/>
+- ESCP Business School MIM (#5 Worldwide)<br/>
+- BITS Pilani (Chemical Engg.)<br/>
+- Ex. Total Energies (Fortune global 500)<br/>
+Integrity Excellence Diversity Inclusion Collaboration<br/>
+• Dr. SAR Hashmi – Senior Advisor Ex. Senior Scientist AMPRI-CSIR<br/>
+• Dr. SKS Rathore – Senior Advisor Ex. Senior Scientist AMPRI-CSIR<br/>
+• Mr. Mridul Khare – Senior Advisor (Renewable Energy)<br/>
+• Mr. Bhavesh Lahori – Project Engineer<br/>
+• Mr.Raj Suman – Plantation Expert<br/>
+• Mr Harsh Patidar<br/>
+</p>
+<h3>Renewable Energy and Climate Change Mitigation</h3>
+<p>Sustainable Options (erstwhile “Environment and Energy Management Group” ) provides
+consultancy services for power generation from clean and renewable sources of energy such as
+bamboo gasification & combustion, small hydro power, solar photo voltaic power systems, wind
+power and village electrification etc. We also provide services for environment clearances of
+projects, conducting environment impact assessment studies, design of effluent treatment plants,
+solid waste management, facilitation of all related statutory clearances etc.
+</p>
+<h3>EEMG PROVIDES THE FOLLOWING SERVICES FOR CLEAN POWER PROJECTS AND INDUSTRIAL
+POLLUTION CONTROL AND ENVIRONMENT MANAGEMENT</h3>
+<h3>• Consulting servicesfor facilitating carbon creditsfor :</h3>
+<p>1. Plantation of bamboo or other appropriate mix of plants<br/>
+2. Biomass based power generation through gasification<br/>
+3. Production of Biochar from waste and it’s applications for climate change mitigation<br/>
+4. Solar PV power<br/>
+5. Small hydro power<br/>
+6. Solar Wind Hybrid Energy<br/>
+7. Aggregating & supplying biomass briquettes/ pellets and raw biomass forswitching to clean
+fuel regime<br/>
+8. Biogas based power plants and compressed biogas plants<br/>
 </p>
 <p>
-We provide the following services:<br/>
-- Carbon footprint assessment , ESG reporting ,nature based solutions and science based targets for net zero
-emissions and Climate Change Mitigation Advisory<br/>
-- Facilitating establishment of net zero emission targets<br/>
-- Development of plantation based biomass supply chain for assured supply of alternate fuel for
-replacement of coal<br/>
-- Planning & implementation of carbon offset projects for net zero carbon emissions and carbon footprint
-reduction<br/>
-- Facilitating production and use of Bamboo Biochar for soil amendment and for replacement of pet coke in
-steel / cement manufacturing<br/>
-- Decarbonisation of manufacturing processes in steel , Cement , chemical and textile sectors<br/>
-- Preparation of feasibility reports and detailed project reports for clean power generation from biomass
-gasification, solar PV and compressed biogas from waste.<br/>
-- Consulting services for production of green hydrogen.<br/>
-- Industrial Pollution Control and Environment Monitoring and Management.<br/>
-- Environment Impact Assessment and Hazardous waste management.<br/>
-- Hydrological and Meteorological studiesfor thermal power plants.<br/>
-- Supply of appropriate biomass to replace coal.<br/>
-- Facilitation of carbon credits from fuel switch and plantations/nature based solutions.<br/>
-- Resettlement and Rehabilitation studies.<br/>
-- Energy transition strategies.<br/>
-- Energy Audits for energy conservation.<br/>
-- Facilitation of bamboo plantation on degraded forest / waste land for biomass fuel.<br/><br/>
-
-With reference to our discussion in your office on 6.02.2024 ,we are pleased to offer our consulting services for
-carbon footprint mapping , science based targets for net zero emissions , nature based solutions and other
-activities for meeting these targets , ESR reporting and Climate Change Mitigation Advisory for various units of
-the Vardhman Group. This would cover all aspects of energy consumption patterns and usage , water consumption
-and efficiency of various processes including waste treatment , opportunities of recycling & energy recovery and
-any other area which may require intervention from carbon footprint perspective .
-We also offer our services for facilitation of carbon credits for your existing and new Solar PV based power plants.
-This would involve survey, analysis and the necessary documentation (project concept note ,baseline study report
-and the PDD) , submission of these to the appropriate platform for processing and follow-up for ensuring all
-process compliance for final issuance of verified carbon credits. Besides this, your existing plantations which have
-proper records of time of plantation and species, are also eligible for carbon credits and we offer our services for
-facilitation of carbon credits for them as well. All these verified carbon credits that can be accrued to you would be
-vital for meeting your net zero carbon emission targets and a better ESG rating..
+• Consulting services for facilitating energy transition, reduction of carbon foot prints and climate
+change mitigation<br/>
+• Preparation of feasibility reports and DPRs for power generation from renewable sources of
+energy<br/>
+• Resource assessment studies for biomass /small hydro /urban waste etc for clean power
+generation<br/>
+• Turnkey services for power generation from renewable and clean sources of energy Power
+evacuation works<br/>
+• Facilitation of power purchase agreement for renewable energy based projects<br/>
+• Facilitation ofsale of power under third party mechanism<br/>
+• Integrated project of Bamboo Gasification for thermal application and power generation with
+captive bamboo plantation<br/>
+• Promotion of waste to energy solutions, solar photovoltaics (ground mounted & solar roof
+top ), AD biogas, Biofuels and Hydrogen production<br/>
+• Hydrological and Meteorological studiesfor thermal power plants<br/>
+• Facilitation of environmental clearance<br/>
+• R&R Studies i.e., preparation of rehabilitation and resettlement plan for project
+affected persons<br/>
+• Energy Audit, preparation of DPR for investment grade project of energy efficiency
+enhancement projects and Implementation on turnkey basis<br/>
+• Environment clearance services<br/>
+• Environment impact assessment and design and monitoring of pollution control facilities<br/>
+• Water audit and water quality monitoring<br/>
+• Integrated solid waste management for waste to energy projects<br/>
+• Integrated rural energy planning for sustainable development<br/>
+• Forest Surveys<br/>
+• Biodiversity assessments<br/>
+• Socio economic assessments<br/>
+• Remote sensing & GIS<br/>
+• Natural Resource Management<br/>
+• ESG reporting, CSR, Carbon footprint assessment and impact assessment<br/>
+• Climate Change Advisory<br/>
+• Life Cycle Assessments<br/>
+• Energy Audits and Consulting<br/>
 </p>
-      
-       </div>
+<h3>MAJOR WORKS DONE IN ENERGY SECTOR</h3>
+<p>1. Preparation of cluster and district level rural energy plans of 24 districts in Madhya Pradesh
+and 1 district in Chhattisgarh under IREP for MP Urja Vikas Nigam and Ministry of nonconventional energy sources of the following districts:
+</p>
+<h3>MADHYA PRADESH</h3>
+<table>
+  <tr>
+    <th>Serial No.</th>
+    <th>District Name</th>
+  </tr>
+  <tr>
+    <td>1.</td>
+    <td>Bhopal</td>
+  </tr>
+  <tr>
+    <td>2.</td>
+    <td>Rajgarh</td>
+  </tr>
+  <tr>
+    <td>3.</td>
+    <td>Shivpuri</td>
+  </tr>
+  <tr>
+    <td>4.</td>
+    <td>Guna</td>
+  </tr>
+  <tr>
+    <td>5.</td>
+    <td>Gwalior</td>
+  </tr>
+  <tr>
+    <td>6.</td>
+    <td>Morena</td>
+  </tr>
+  <tr>
+    <td>7.</td>
+    <td>Narsinghpur</td>
+  </tr>
+  <tr>
+    <td>8.</td>
+    <td>Jabalpur</td>
+  </tr>
+  <tr>
+    <td>9.</td>
+    <td>Katni</td>
+  </tr>
+  <tr>
+    <td>10.</td>
+    <td>Dindori</td>
+  </tr>
+  <tr>
+    <td>11.</td>
+    <td>Balaghat</td>
+  </tr>
+  <tr>
+    <td>12.</td>
+    <td>Chhindwara</td>
+  </tr>
+  <tr>
+    <td>13.</td>
+    <td>Mandla</td>
+  </tr>
+  <tr>
+    <td>14.</td>
+    <td>Seoni</td>
+  </tr>
+  <tr>
+    <td>15.</td>
+    <td>Satna</td>
+  </tr>
+  <tr>
+    <td>16.</td>
+    <td>Rewa</td>
+  </tr>
+  <tr>
+    <td>17.</td>
+    <td>Sidhi</td>
+  </tr>
+  <tr>
+    <td>18.</td>
+    <td>Umariya</td>
+  </tr>
+  <tr>
+    <td>19.</td>
+    <td>Shahdol</td>
+  </tr>
+  <tr>
+    <td>20.</td>
+    <td>Anuppur</td>
+  </tr>
+  <tr>
+    <td>21.</td>
+    <td>Khargone</td>
+  </tr>
+  <tr>
+    <td>22.</td>
+    <td>Ujjain</td>
+  </tr>
+  <tr>
+    <td>23.</td>
+    <td>Barwani</td>
+  </tr>
+  <tr>
+    <td>24.</td>
+    <td>Vidisha</td>
+  </tr>
+</table>
+<h3>CHHATTISGARH</h3>
+<p>Mahasamund district</p>
+
+      </div>
       <WebFooter />
       </>
         
