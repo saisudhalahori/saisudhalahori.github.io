5b7b1dccf92f49240aaad9458f9c4794346335fa
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-04-09 18:43:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
-// @vite.config.ts
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite"
-import ViteFonts from "vite-plugin-fonts"
-import reactRefresh from "@vitejs/plugin-react-refresh"
-import type { UserConfig as VitestUserConfigInterface } from "vitest/config"
-
-const vitestConfig: VitestUserConfigInterface = {
-	test: {
-		globals: true,
-		environment: "jsdom",
-		include: ["src/**/*.spec.*"],
-		setupFiles: "./src/test/setup.ts",
-		clearMocks: true,
-		watch: false,
-	},
-}
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		reactRefresh(),
-		ViteFonts({
-			google: {
-				families: ["Roboto"],
-			},
-		}),
-	],
-	test: vitestConfig.test,
+  
+  plugins: [react()],
+  base:"/",
+
 })
+
