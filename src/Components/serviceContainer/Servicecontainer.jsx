3e1c858d0503081d2ebe4cc8f7504c59d40e63a3
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-04-15 17:23:13 +0530

    Update Servicecontainer.jsx

--- src/Components/serviceContainer/Servicecontainer.jsx
+++ src/Components/serviceContainer/Servicecontainer.jsx
@@ -2,32 +2,69 @@ import React from 'react'
 import './Servicecontainer.scss'
 const Servicecontainer = () => {
   return (
+    <>
+    <div className='service_main_comtainer'>
+      <h1>Services</h1>
+    </div>
     <div className='services_container'>
-    <h1 className='service_heading1'><span>Service For</span> Bamboo Technology Center<br/> <img className='service_cntainer_image'  src="./leafimage.png" alt="" /></h1>
+    <h1 className='service_heading1'><span>Our services</span><br/> <img className='service_cntainer_image'  src="\leafimage.png" alt="" /></h1>
    <div className="service_flex_container">
     <div  className="service_flex_container1">
-    <h2 className='service_heading2'>Training for working with bamboo for promoting sustainable employment opportunities</h2>
-    <p className='service_para'>We provide training to unemployed youth specially women in rural areas in various applications of bamboo in construction and handicraft sector. The training covers plantation , harvesting , treatment , processing and production of value added products such as roofing sheets , flooring tiles , scaffolding materials , furniture , baskets & mat weaving and use of bamboo as biomass for power generation and large scale plantation of bamboo on wasteland .Such training programs open up additional avenues for employment for rural poor and promote eco friendly sustainable development.</p>
-<h2 className='service_heading2'>Bamboo based trades for skill development for sustainable employment</h2>
-<p className='service_para'>1.Bamboo Propagation through intensive cultivation of bamboo on waste land and production of organic manure from bamboo plantation<br/>
-2. Appropriate Plantation techniques<br/>
-3. Preservation and treatment of Bamboo for longer life<br/>
-4.Primary processing of Bamboo<br/>
-5. Bamboo Handicrafts and Furniture<br/>
-6. Bamboo based Structure and Buildings<br/>
-7. Bamboo based value added products<br/>
-• Round sticks of bamboo for agarbatti<br/>
-• Bamboo sticks brooms<br/>
-• Bamboo chick weaving<br/>
-• Bamboo charcoal soap making<br/>
-• Poly house structures using treated bamboo poles.</p>
+    <h2 className='service_heading2'>services for carbon footprint mapping, science based net zero carbon emission
+targets and development of nature based carbon offset projects for meeting them through facilitation of carbon
+credits.
+</h2>
+  <p className='service_para'>Sustainable Options is a Bhopal, Madhya Pradesh, India based organization working for promotion of clean energy
+technologies for renewable energy generation, conservation of environment and promotion of bamboo basedtechnologies for sustainable development for over 30 years. We provide consultancy services for carbon footprint
+mapping , carbon offset projects for climate change mitigation ,facilitation of carbon credits , power generation
+from clean and renewable sources of energy such as bamboo gasification & combustion, small hydro power, solar
+photo voltaic power systems, wind power , production of green hydrogen and alternative fuels for replacing fossil
+fuels.</p>
+<h2 className='service_heading2'>We provide the following services:</h2>
+<p className='service_para'>- Carbon footprint assessment , ESG reporting ,nature based solutions and science based targets for net zero
+emissions and Climate Change Mitigation Advisory.<br/>
+- Facilitating establishment of net zero emission targets.<br/>
+- Development of plantation based biomass supply chain for assured supply of alternate fuel for
+replacement of coal.<br/>
+- Planning & implementation of carbon offset projects for net zero carbon emissions and carbon footprint
+reduction.<br/>
+- Facilitating production and use of Bamboo Biochar for soil amendment and for replacement of pet coke in
+steel / cement manufacturing.<br/>
+- Decarbonisation of manufacturing processes in steel , Cement , chemical and textile sectors.<br/>
+- Preparation of feasibility reports and detailed project reports for clean power generation from biomass
+gasification, solar PV and compressed biogas from waste.<br/>
+- Consulting services for production of green hydrogen.<br/>
+- Industrial Pollution Control and Environment Monitoring and Management.<br/>
+- Environment Impact Assessment and Hazardous waste management.<br/>
+- Hydrological and Meteorological studiesfor thermal power plants.<br/>
+- Supply of appropriate biomass to replace coal.<br/>
+- Facilitation of carbon credits from fuel switch and plantations/nature based solutions.<br/>
+- Resettlement and Rehabilitation studies.<br/>
+- Energy transition strategies.<br/>
+- Energy Audits for energy conservation.<br/>
+- Facilitation of bamboo plantation on degraded forest / waste land for biomass fuel.</p>
+
+<p className='service_para'>With reference to our discussion in your office on 6.02.2024 ,we are pleased to offer our consulting services for
+carbon footprint mapping , science based targets for net zero emissions , nature based solutions and other
+activities for meeting these targets , ESR reporting and Climate Change Mitigation Advisory for various units of
+the Vardhman Group. This would cover all aspects of energy consumption patterns and usage , water consumption
+and efficiency of various processes including waste treatment , opportunities of recycling & energy recovery and
+any other area which may require intervention from carbon footprint perspective .</p>
+
+<p className='service_para'>We also offer our services for facilitation of carbon credits for your existing and new Solar PV based power plants.
+This would involve survey, analysis and the necessary documentation (project concept note ,baseline study report
+and the PDD) , submission of these to the appropriate platform for processing and follow-up for ensuring all
+process compliance for final issuance of verified carbon credits. Besides this, your existing plantations which have
+proper records of time of plantation and species, are also eligible for carbon credits and we offer our services for
+facilitation of carbon credits for them as well. All these verified carbon credits that can be accrued to you would be
+vital for meeting your net zero carbon emission targets and a better ESG rating..
+</p>
    </div>
  
-   <div className="service_flex_container2">
-  <img className='service_img'  src="\servicesbamboo.jpg" alt="" />
-  </div>
+ 
    </div>
    </div>
+   </>
   )
 }
 
