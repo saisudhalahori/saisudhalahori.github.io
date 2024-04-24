876763fe65fa653fa154c49e15f1f1eafa42ca0d
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-04-24 12:06:04 +0530

    Update Adminlogin.jsx

--- src/Pages/adminlogin/Adminlogin.jsx
+++ src/Pages/adminlogin/Adminlogin.jsx
@@ -1,53 +1,45 @@
 // Login.js
-import React, { useState } from 'react';
+import React, { useState  } from 'react';
 import './Adminlogin.scss';
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword  } from "firebase/auth";
 import { auth } from '../../firebase';
 import { useNavigate } from 'react-router-dom';
+import { authDeclined, authorized  } from '../../Redux/Authentication';
 import { useDispatch } from 'react-redux';
-import { authDeclined, authorized } from '../../Redux/Authentication';
+
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const navigate = useNavigate()
+ const dispatch = useDispatch()
 
   const handleLogin = (e) => {
     e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        
-        const user = userCredential.user;
-      console.log(user)
+    
+    signInWithEmailAndPassword (auth, email, password)
+    .then((userCredential) => {
+      
+    
+    const user = userCredential.user;
+    dispatch(authorized({email:user.email,uid:user.uid}))
+    navigate('/admin')
 
-        dispatch(authorized(true));
-        navigate('/admin');
-      })
-      .catch((error) => {
-
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        
-        if (errorCode === 'auth/wrong-password') {
-          setError('Incorrect password. Please try again.');
-        } else if (errorCode === 'auth/user-not-found') {
-          setError('User not found. Please check your email.');
-        } else {
-          setError(errorMessage);
-          dispatch(authDeclined(errorCode));
-        }
-      });
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+   dispatch(authDeclined(errorCode))
+    // ..
+  });
   };
 
   return (
     <div className="login-container">
       <form onSubmit={handleLogin} className="login-form">
-        <h2>Login <br/><img src="\leafimage.png"/></h2>
-     
+        <h2>Login</h2>
         <div className="input-group">
-        {error && <p className="error-message">{error}</p>}
           <label htmlFor="email">Email:</label>
           <input
             type="email"
