90493dddbf7390b86a2762a16d4a0ca0b767471f
saisudhalahori <131282718+sustainableoption@users.noreply.github.com>
2024-05-16 15:07:21 +0530

    Update AboutUs.jsx

--- src/webpages/aboutus/AboutUs.jsx
+++ src/webpages/aboutus/AboutUs.jsx
@@ -18,38 +18,7 @@ const AboutUs = () => {
         <p>Sustainable Options is a Bhopal based organization working for promotion of clean energy
 technologies for renewable energy generation, conservation of environment and promotion of
 bamboo based-technologies for sustainable development for over 30 years.</p>
-<h3>Mission, Vision and Core Values</h3>
-<p>Our Mission: Our raison d'être is to reimagine and restructure existing strategies and use of
-resources to support our clients with their Transition to Clean Sources of Energy , ESG, SDG & Net
-Zero Carbon goals through sustainable and profitable solutions
-Our Vision: To establish ourselves as the market leaders in Switching to Clean Fuel Regime,
-Renewable Energy Management ,Carbon Credit Facilitation and Net Zero Carbon achievement for
-our clients and to promote strategies for high-growth Circular Economy business models
-</p>
-<h3>The Team</h3>
-<p>•Rahul Saksena – Chief Executive Officer<br/>
-- Founder of EEMG & Sustainable Options<br/>
-- Empanelled with Consultancy Development Center, Govt. of India (RE Projects)<br/>
-- Consultant for RE Power Projects<br/>
-- Consultant to IIT Roorkee<br/>
-- Supply Chain Management Expert with National Bamboo Mission and M.P. State Bamboo
-Mission<br/>
-- Biomass Supply Chain Management Expert with the World Resource Institute, New Delhi<br/>
-- Empanelled Consultant with M.P. Urja Vikas Nigam for Climate Change Mitigation & RE
-Projects<br/>
-- Consultant for Carbon Footprint Assessment & Reduction Strategies<br/>
-• Dev Raghav – Managing Director<br/>
-- ESCP Business School MIM (#5 Worldwide)<br/>
-- BITS Pilani (Chemical Engg.)<br/>
-- Ex. Total Energies (Fortune global 500)<br/>
-Integrity Excellence Diversity Inclusion Collaboration<br/>
-• Dr. SAR Hashmi – Senior Advisor Ex. Senior Scientist AMPRI-CSIR<br/>
-• Dr. SKS Rathore – Senior Advisor Ex. Senior Scientist AMPRI-CSIR<br/>
-• Mr. Mridul Khare – Senior Advisor (Renewable Energy)<br/>
-• Mr. Bhavesh Lahori – Project Engineer<br/>
-• Mr.Raj Suman – Plantation Expert<br/>
-• Mr Harsh Patidar<br/>
-</p>
+
 <h3>Renewable Energy and Climate Change Mitigation</h3>
 <p>Sustainable Options (erstwhile “Environment and Energy Management Group” ) provides
 consultancy services for power generation from clean and renewable sources of energy such as
@@ -70,8 +39,6 @@ POLLUTION CONTROL AND ENVIRONMENT MANAGEMENT</h3>
 7. Aggregating & supplying biomass briquettes/ pellets and raw biomass forswitching to clean
 fuel regime<br/>
 8. Biogas based power plants and compressed biogas plants<br/>
-</p>
-<p>
 • Consulting services for facilitating energy transition, reduction of carbon foot prints and climate
 change mitigation<br/>
 • Preparation of feasibility reports and DPRs for power generation from renewable sources of
@@ -107,115 +74,7 @@ enhancement projects and Implementation on turnkey basis<br/>
 • Life Cycle Assessments<br/>
 • Energy Audits and Consulting<br/>
 </p>
-<h3>MAJOR WORKS DONE IN ENERGY SECTOR</h3>
-<p>1. Preparation of cluster and district level rural energy plans of 24 districts in Madhya Pradesh
-and 1 district in Chhattisgarh under IREP for MP Urja Vikas Nigam and Ministry of nonconventional energy sources of the following districts:
-</p>
-<h3>MADHYA PRADESH</h3>
-<table>
-  <tr>
-    <th>Serial No.</th>
-    <th>District Name</th>
-  </tr>
-  <tr>
-    <td>1.</td>
-    <td>Bhopal</td>
-  </tr>
-  <tr>
-    <td>2.</td>
-    <td>Rajgarh</td>
-  </tr>
-  <tr>
-    <td>3.</td>
-    <td>Shivpuri</td>
-  </tr>
-  <tr>
-    <td>4.</td>
-    <td>Guna</td>
-  </tr>
-  <tr>
-    <td>5.</td>
-    <td>Gwalior</td>
-  </tr>
-  <tr>
-    <td>6.</td>
-    <td>Morena</td>
-  </tr>
-  <tr>
-    <td>7.</td>
-    <td>Narsinghpur</td>
-  </tr>
-  <tr>
-    <td>8.</td>
-    <td>Jabalpur</td>
-  </tr>
-  <tr>
-    <td>9.</td>
-    <td>Katni</td>
-  </tr>
-  <tr>
-    <td>10.</td>
-    <td>Dindori</td>
-  </tr>
-  <tr>
-    <td>11.</td>
-    <td>Balaghat</td>
-  </tr>
-  <tr>
-    <td>12.</td>
-    <td>Chhindwara</td>
-  </tr>
-  <tr>
-    <td>13.</td>
-    <td>Mandla</td>
-  </tr>
-  <tr>
-    <td>14.</td>
-    <td>Seoni</td>
-  </tr>
-  <tr>
-    <td>15.</td>
-    <td>Satna</td>
-  </tr>
-  <tr>
-    <td>16.</td>
-    <td>Rewa</td>
-  </tr>
-  <tr>
-    <td>17.</td>
-    <td>Sidhi</td>
-  </tr>
-  <tr>
-    <td>18.</td>
-    <td>Umariya</td>
-  </tr>
-  <tr>
-    <td>19.</td>
-    <td>Shahdol</td>
-  </tr>
-  <tr>
-    <td>20.</td>
-    <td>Anuppur</td>
-  </tr>
-  <tr>
-    <td>21.</td>
-    <td>Khargone</td>
-  </tr>
-  <tr>
-    <td>22.</td>
-    <td>Ujjain</td>
-  </tr>
-  <tr>
-    <td>23.</td>
-    <td>Barwani</td>
-  </tr>
-  <tr>
-    <td>24.</td>
-    <td>Vidisha</td>
-  </tr>
-</table>
-<h3>CHHATTISGARH</h3>
-<p>Mahasamund district</p>
+
 
       </div>
       <WebFooter />
